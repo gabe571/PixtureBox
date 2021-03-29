9821c2805044eab939c707e9f9af3c886dac5e57
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-29 09:51:13 -0700

    fetchMovies

--- src/service/index.js
+++ src/service/index.js
@@ -2,8 +2,74 @@ import axios from 'axios'
 
 const apiKey = '63cad23fc5d27c914717d084f3379dd9';
 const url = 'https://api.themoviedb.org/3';
-const nowPlayingUrl= '';
-const topratedUrl = '';
-const movieUrl = '';
-const perosnal = '';
+const nowPlayingUrl = `${url}/movie/now_playing`;
+const topratedUrl = `${url}/movie/top_rated`;
+const movieUrl = `${url}/movie`;
+const genreUrl = `${url}/genre/movie/list`;
+const moviesUrl = `${url}/discover/movie`;
+const personUrl = `${url}/trending/person/week`;
+ 
+export const fetchMovies = async () => {
+    try {
+        const { data } = await axios.get(nowPlayingUrl, {
+            params: {
+                api_key: apiKey,
+                language: 'en_US',
+                page: 1
+            }
+        })
 
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
+
+        return modifiedData;
+    } catch (error) { }
+}
+
+export const fetchGenre = () => {
+
+}
+
+export const fetchMovieBoyGenre = () => {
+
+}
+
+export const fetchPersons = () => {
+    
+}
+
+export const fetchTopratedMovie = () => {
+
+}
+
+export const fetchMovieDetail = () => {
+    
+}
+
+export const fetchMovieVideos = () => {
+
+}
+
+export const fetchCasts = () => {
+    
+}
+
+export const fetchMovieVideos = () => {
+
+}
+
+export const fetchCasts = () => {
+    
+}
+
+export const fetchSimilarMovie = () => {
+    
+}
