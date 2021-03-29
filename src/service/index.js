9aa5bb4175ea37e8dff329264027ef331cfc0c83
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-29 09:27:36 -0700

    axios calls

---
+++ src/service/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const apiKey = '63cad23fc5d27c914717d084f3379dd9';
+const url = 'https://api.themoviedb.org/3';
+const nowPlayingUrl= '';
+const topratedUrl = '';
+const movieUrl = '';
+const perosnal = '';
+
