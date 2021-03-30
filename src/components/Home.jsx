516fd696941759ba567b896939b18650eeff6611
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-30 09:14:21 -0700

    movieByGenre

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect }  from 'react';
 import RBCarousel from 'react-bootstrap-carousel';
-import { fetchMovies, fetchGenre } from "../service";
-import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css'
+import { fetchMovies, fetchGenre, fetchMovieByGenre } from "../service";
+import { Link } from 'react-router-dom';
+import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
 
 export function Home() {
     const [nowPlaying, setNowPlaying] = useState([]);
     const [genres, setGenres] = useState([]);
+    const [movieByGenre, setMovieByGenre] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
             setNowPlaying(await fetchMovies());
             setGenres(await fetchGenre());
+            setMovieByGenre(await fetchMovieByGenre());
         };
         fetchAPI();
     },[]);
@@ -43,9 +46,21 @@ export function Home() {
                     {item.name}
                 </button>
             </li>
+        );
+    });
+
+    const movieList = movieByGenre.slice(0, 4).map((item, index) => {
+        return (
+            <div className='col-md-3 col-sm-6' key={index}>
+                <div className='card>'>
+                <Link to={`/movie/${item.id}`}>
+                    <img className='img-fluid' src={item.poster} alt={item.title}></img>
+                </Link>
+            </div>    
+        </div>
         )
     })
-    return(
+    return (
         <div>
             <h1 className='logo'> PixtureBox </h1>
         <div className="container">
@@ -68,6 +83,7 @@ export function Home() {
                         </ul>
                     </div>
                 </div>
+                <div className='row mt-3'>{movieList}</div>
             </div>
         </div>
         </div>
--- src/service/index.js
+++ src/service/index.js
@@ -50,9 +50,30 @@ export const fetchGenre = async () => {
         return modifiedData;
     } catch (error) { }
 }
-// export const fetchMovieBoyGenre = () => {
+export const fetchMovieByGenre = async (genre_id) => {
+    try {
+        const { data } = await axios.get(moviesUrl, {
+            params: {
+                api_key: apiKey,
+                language: 'en_US',
+                page: 1,
+                with_genres: genre_id
+            }
+        })
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
 
-// }
+        return modifiedData;
+    } catch (error) { }
+}
 
 // export const fetchPersons = () => {
     
