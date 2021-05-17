c4807167f640e5cd8b1e95fcad1a65165170c88a
Gabe571 <gabriel.castro1028@gmail.com>
2021-05-17 16:53:37 -0700

    font styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
 
 function Navbar() {
 
     return(
         <div>
              <NavLink to='/' className='Nav_link' activeStyle={{ color: 'tomato'}}><li>PixtureBox</li></NavLink>
-             <NavLink to='/profile' className='Nav_link_two' activeStyle={{ color: 'tomato'}}><li>Profile</li></NavLink>
+             <NavLink to='/profile' className='Nav_link_two' activeStyle={{ color: 'tomato'}}><li><FaUserCircle size='50px' /></li></NavLink>
         </div>
     )
 }
